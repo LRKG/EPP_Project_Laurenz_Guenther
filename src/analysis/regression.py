a1dfcbae27332c6a14225def7ca8414df78cd340
Laurenz Guenther <s3laguen@uni-bonn.de>
2019-03-05 19:38:25 +0100

    I edited the regression-file. There I solved a problem regarding the classification algorithm. Before it always classified everyithing as non-islam.

--- src/analysis/regression.py
+++ src/analysis/regression.py
@@ -1,8 +1,3 @@
-
-
-
-
- ################################################################################################################
 import pandas as pd
 from bld.project_paths import project_paths_join as ppj
 import seaborn as sns
@@ -32,7 +27,7 @@ max_row=sheet.max_row
 # Create a tuple containing for each article the words as an array and for each type of terrorism an indicator.
 tuple = []
 for row_id in range(2, max_row+1): 
-    tuple.append(([x.strip() for x in str(sheet.cell(row=row_id, column=1).value).split(';')], str(sheet.cell(row=row_id, column=2).value), str(sheet.cell(row=row_id, column=3).value), str(sheet.cell(row=row_id, column=4).value), str(sheet.cell(row=row_id, column=5).value)))
+    tuple.append(([x.strip() for x in str(sheet.cell(row=row_id, column=1).value).split(';')], str(sheet.cell(row=row_id, column=4).value)))
 
 # Randomise the order of the articles.
 random.shuffle(tuple)
@@ -46,15 +41,15 @@ for article in range(0, len(tuple)):
 # Generate a frequency distribution of those words i.e. count for each how often it occured.      
 all_words = nltk.FreqDist(all_words)    
 # Make a list with number of occurences for the words that occur most often up to 3000th most often.
-word_features = list(all_words.keys())[:500]
+word_features = list(all_words.keys())[:10000]
     
 
-def find_features(article_id):
+def find_features(article):
     #Tell for each of the 3000 words that occur most often
     #whether it occurs in a given article or not.
     #Input: Id of an article.
     #Output: Dictionary containing as keys the 3000 words that occur most often in all articles. Values are boolean and indicate whether the given article contains the word or not (True/False)
-    words = set(tuple[article_id][0])
+    words = set(article)
     features = {}
     for w in word_features:
         features[w] = (w in words)        
@@ -66,8 +61,8 @@ def find_features(article_id):
 # that generally occur most often is contained in each article and one indicator
 # for the kind of terrorism, for each kind of terrorism. 
 feature_sets = []
-for i in range(2, len(tuple)):
-    feature_sets.append((find_features(i), str(sheet.cell(row=i, column=4).value)))
+for i in range(0, len(tuple)-1):
+    feature_sets.append((find_features(tuple[i][0]), str(sheet.cell(row=i+2, column=4).value)))
 
 training_set = feature_sets[:int(len(feature_sets)/2)]
 testing_set = feature_sets[int(len(feature_sets)/2):]
@@ -75,6 +70,7 @@ testing_set = feature_sets[int(len(feature_sets)/2):]
 classifier = nltk.NaiveBayesClassifier.train(training_set)
 #print("Original naive Bayes Algorithm Accuracy in percent:", (nltk.classify.accuracy(classifier, testing_set)*100))
 #classifier.show_most_informative_features(10)
+#classifier.labels()
 
 
 
@@ -82,23 +78,22 @@ classifier = nltk.NaiveBayesClassifier.train(training_set)
 #filepath="C:/Users/laure/Desktop/RESEARCH/Media_Bias/code/full_sparse.xlsx"
 
 # Load the training data.
-wb=load_workbook(ppj("OUT_DATA", "all_articles.xlsx"))
-sheet=wb.active
-max_row=sheet.max_row
+wb_2=load_workbook(ppj("OUT_DATA", "all_articles.xlsx"))
+sheet_2=wb_2.active
+max_row_2=sheet_2.max_row
 
 
-tuple = []
-for row_id in range(2, max_row+1): 
-    tuple.append(([x.strip() for x in str(sheet.cell(row=row_id, column=2).value).split(';')]))#, datetime.strptime(str(sheet.cell(row=row_id, column=1).value), '%Y/%m/%d')))
+tuple_2 = []
+for row_id in range(2, max_row_2+1): 
+    tuple_2.append(([x.strip() for x in str(sheet_2.cell(row=row_id, column=2).value).split(';')]))#, datetime.strptime(str(sheet.cell(row=row_id, column=1).value), '%Y/%m/%d')))
 
-feature_sets = []
-for i in range(0, int(len(tuple))):
-    feature_sets.append((find_features(i)))
+feature_sets_2 = []
+for i in range(0, int(len(tuple_2)-1)):
+    feature_sets_2.append((find_features(tuple_2[i])))
 
 
-classifications_islam = classifier.classify_many(feature_sets)
-
-#print(classifications_islam)
+classifications_islam = classifier.classify_many(feature_sets_2)
+print(classifications_islam)
 
 
     
@@ -109,19 +104,27 @@ classifications_islam = classifier.classify_many(feature_sets)
 
 
 
-islam_year = 0
+
 other_year = 0
-year_old = 0
+year_old = datetime.strptime(sheet_2.cell(row=0 + 2, column=1).value, '%Y/%m/%d').year
+year_new = 0
+islam_year = 0
 dict = {'year': [], "islam": []}
-for row_id in range(0, max_row-4): 
-    if (datetime.strptime(sheet.cell(row=row_id + 2, column=1).value, '%Y/%m/%d').year > year_old):
-        year_old = datetime.strptime(sheet.cell(row=row_id + 2, column=1).value, '%Y/%m/%d').year
-        dict["year"].append(year_old)
-        dict["islam"].append(islam_year)      
+for row_id in range(0, max_row_2-2):     
+    if classifications_islam[row_id] == "islam":      
+        islam_year =islam_year + 1     
+    if (datetime.strptime(sheet_2.cell(row=row_id + 2, column=1).value, '%Y/%m/%d').year > year_old):
+        year_new = datetime.strptime(sheet_2.cell(row=row_id + 2, column=1).value, '%Y/%m/%d').year
         
-    else: 
-        if classifications_islam[row_id] == "islam":
-            islam_year =islam_year + 1            
+        dict["islam"].append(islam_year)         
+        dict["year"].append(year_old)
+        islam_year = 0
+        year_old = year_new
+ 
+dict["islam"].append(islam_year)         
+dict["year"].append(year_old)       
+       
+    
     
 df = pd.DataFrame(data=dict)
 
@@ -131,6 +134,7 @@ df = pd.DataFrame(data=dict)
 plt.plot( 'year', 'islam', data=df, marker='', color='red', linewidth=2, linestyle='dashed', label="number of reports on islamist terrorist attacks")
 #plt.plot( 'year', 'other', data=df, marker='', color='red', linewidth=2, linestyle='_ _', label="toto")
 plt.legend()
+
 
 plt.savefig(ppj("OUT_FIGURES", "line_chart.pdf"), delimiter=",")
 
